8c78ebbe233b344ae36c84f18bee88cbee944296
Васякин Данил Евгеньевич <Danil.Vassyakin@kaspi.kz>
2022-08-25 14:09:57 +0600

    feat: add queue rotation

--- src/main/java/Queue.java
+++ src/main/java/Queue.java
@@ -12,20 +12,37 @@ public class Queue<T>
 
     public void enqueue(T item)
     {
-        list.add(0, item);
+        list.add(item);
     }
 
     public T dequeue()
     {
         if (list.size() == 0) { return null; }
-        T element = list.get(0);
-        list.remove(0);
+        int index = list.size() - 1;
+        T element = list.get(index);
+        list.remove(index);
         return element;
     }
 
     public int size()
     {
         return list.size();
+    }
+
+    public void rotate(int n) {
+        n = n % size();
+
+        ArrayList<T> e = new ArrayList<>();
+        for (int i = n + 1; i < size(); i++) {
+            e.add(list.get(i));
+        }
+
+        int size = e.size();
+        for (int i = 0; i < list.size() - size; i++) {
+            e.add(list.get(i));
+        }
+
+        list = e;
     }
 
 }
--- src/test/java/QueueTest.java
+++ src/test/java/QueueTest.java
@@ -24,4 +24,31 @@ class QueueTest {
         assertNull(q.dequeue());
     }
 
+    @Test
+    void test_rotation() {
+        Queue<Integer> q = new Queue<>();
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+
+        q.rotate(1);
+        assertEquals(2, q.dequeue());
+        assertEquals(1, q.dequeue());
+        assertEquals(3, q.dequeue());
+
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+        q.enqueue(4);
+        q.enqueue(5);
+
+        q.rotate(2);
+        assertEquals(3, q.dequeue());
+        assertEquals(2, q.dequeue());
+        assertEquals(1, q.dequeue());
+        assertEquals(5, q.dequeue());
+        assertEquals(4, q.dequeue());
+
+    }
+
 }
