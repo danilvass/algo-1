2cf2681d5e336ac4cfbce0e47a75d8ea5e226fcd
Васякин Данил Евгеньевич <Danil.Vassyakin@kaspi.kz>
2022-09-02 14:42:35 +0600

    feat: ordered list impl

---
+++ src/main/java/OrderedList.java
@@ -0,0 +1,223 @@
+import java.util.*;
+
+class Node<T>
+{
+    public T value;
+    public Node<T> next, prev;
+
+    public Node(T _value)
+    {
+        value = _value;
+        next = null;
+        prev = null;
+    }
+}
+
+public class OrderedList<T>
+{
+    public Node<T> head, tail;
+    private boolean _ascending;
+
+    private final int EQUAL_COMPARE = 0;
+
+    public OrderedList(boolean asc)
+    {
+        head = null;
+        tail = null;
+        _ascending = asc;
+    }
+
+    private int nextCompareResult() {
+        return _ascending ? 1 : -1;
+    }
+
+    private int prevCompareResult() {
+        return _ascending ? -1 : 1;
+    }
+
+    public int compare(T v1, T v2)
+    {
+        if (v1 instanceof Number) {
+            return ((Integer)v1).compareTo(((Integer)v2));
+        }
+        return 0;
+    }
+
+    public void add(T value)
+    {
+        if (head == null) {
+            this.head = new Node<>(value);
+            this.tail = this.head;
+            return;
+        }
+
+        Node<T> newNode = new Node<>(value);
+        Node<T> currentNode = findClosestNode(value);
+
+        int compareResult = compare(currentNode.value, value);
+
+        if (compareResult == EQUAL_COMPARE || compareResult == nextCompareResult()) {
+            //Inserting new value before founded node
+            if (currentNode == this.head) {
+                insertToHead(value);
+                return;
+            }
+
+            Node<T> prev = currentNode.prev;
+            newNode.prev = prev;
+            currentNode.prev = newNode;
+            newNode.next = currentNode;
+            if (prev != null) { prev.next = newNode; }
+        } else {
+            //Inserting new value after founded node
+
+            if (currentNode == this.tail) {
+                insertToTail(value);
+                return;
+            }
+
+            Node<T> next = currentNode.next;
+            newNode.next = next;
+            currentNode.next = newNode;
+            newNode.prev = currentNode;
+            if (next != null) { next.prev = newNode; }
+        }
+    }
+
+    public Node<T> find(T val)
+    {
+        Node<T> currentNode = findClosestNode(val);
+        if (currentNode.value == val) {
+            return currentNode;
+        } else if (currentNode.next != null && compare(currentNode.next.value, val) == EQUAL_COMPARE) {
+            return currentNode.next;
+        } else if (currentNode.prev != null && compare(currentNode.prev.value, val) == EQUAL_COMPARE) {
+            return currentNode.prev;
+        }
+        return null;
+    }
+
+    public void delete(T val)
+    {
+        Node<T> node = find(val);
+        if (node == null) { return; }
+        Node<T> next = node.next;
+        Node<T> prev = node.prev;
+        if (next != null && prev != null) {
+            next.prev = prev;
+            prev.next = next;
+            return;
+        }
+        if (next != null) {
+            next.prev = null;
+            this.head = next;
+            return;
+        }
+        if (prev != null) {
+            prev.next = null;
+            this.tail = prev;
+        }
+    }
+
+    public void clear(boolean asc)
+    {
+        _ascending = asc;
+        ArrayList<Node<T>> list = getAll();
+        clearAll();
+        for (Node<T> element: list) {
+            add(element.value);
+        }
+    }
+
+    public int count()
+    {
+        int _count = 0;
+        Node node = this.head;
+        while (node != null) {
+            node = node.next;
+            _count++;
+        }
+        return _count;
+    }
+
+    ArrayList<Node<T>> getAll() {
+        ArrayList<Node<T>> r = new ArrayList<Node<T>>();
+        Node<T> node = head;
+        while(node != null)
+        {
+            r.add(node);
+            node = node.next;
+        }
+        return r;
+    }
+
+    //MARK: - processing helpers
+    private void clearAll() {
+        while(this.head != null) {
+            this.head = this.head.next;
+        }
+        this.tail = null;
+    }
+
+    private Node<T> findMiddle(Node<T> head, boolean next, int size) {
+        Node<T> _head = head;
+        int i = 0;
+        while (i != size) {
+            if (next) {
+                _head = _head.next;
+            } else {
+                _head = _head.prev;
+            }
+            i++;
+        }
+        return _head;
+    }
+
+    private Node<T> findClosestNode(T value) {
+        if (compare(head.value, value) == nextCompareResult()) {
+            return this.head;
+        }
+
+        if (this.tail != null && compare(tail.value, value) == prevCompareResult()) {
+            return this.tail;
+        }
+
+        int _count = this.count();
+        _count = (int) Math.ceil((double)_count / 2.0);
+
+        Node<T> mid = findMiddle(this.head, true, _count);
+        Node<T> _notNullMid = mid;
+
+        while(mid != null && _count > 1) {
+            _count = (int) Math.ceil((double)_count / 2.0);
+
+            if (compare(mid.value, value) == EQUAL_COMPARE) {
+                return mid;
+            } else if (compare(mid.value, value) == nextCompareResult()) {
+                mid = findMiddle(mid, false, _count);
+            } else {
+                mid = findMiddle(mid, true, _count);
+            }
+            if (mid != null) { _notNullMid = mid; }
+        }
+
+        return _notNullMid;
+    }
+
+    private void insertToHead(T value) {
+        Node<T> newHead = new Node<>(value);
+        Node<T> oldHead = this.head;
+        this.head = newHead;
+        newHead.next = oldHead;
+        oldHead.prev = newHead;
+    }
+
+    private void insertToTail(T value) {
+        Node<T> newTail = new Node<>(value);
+        Node<T> oldTail = this.tail;
+        this.tail = newTail;
+        newTail.prev = oldTail;
+        oldTail.next = newTail;
+    }
+
+}
---
+++ src/test/java/OrderedListTest.java
@@ -0,0 +1,202 @@
+import com.sun.tools.corba.se.idl.constExpr.Or;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderedListTest {
+
+    @Test
+    void test_compare_ints() {
+        OrderedList<Integer> sut = createSUTInt(true);
+        assertEquals(0, sut.compare(1, 1));
+        assertEquals(0, sut.compare(2, 2));
+        assertEquals(1, sut.compare(1, 0));
+        assertEquals(-1, sut.compare(0, 1));
+    }
+
+    @Test
+    void test_compare_strings() {
+        OrderedList<String> sut = createSUTStr(true);
+        assertEquals(0, sut.compare("1", "1"));
+        assertEquals(0, sut.compare("2", "2"));
+        assertEquals(1, sut.compare("1", "0"));
+        assertEquals(-1, sut.compare("0", "1"));
+    }
+
+    @Test
+    void test_findNodeWithBinarySearch() {
+        OrderedList<Integer> sut = createSUTInt(true);
+        assertEquals(9, sut.count());
+
+        sut.add(0);
+        assertEquals(0, sut.head.value);
+        sut.add(1);
+        assertEquals(1, sut.head.next.value);
+        sut.add(10);
+        assertEquals(10, sut.tail.value);
+        sut.add(99);
+        assertEquals(99, sut.tail.value);
+        sut.add(88);
+        assertEquals(88, sut.tail.prev.value);
+
+        assertEquals(14, sut.count());
+        System.out.println(sut.str());
+    }
+
+    @Test
+    void test_find() {
+        OrderedList<Integer> sut = createSUTInt(true);
+        assertNull(sut.find(0));
+
+        assertEquals(1, sut.find(1).value);
+        assertEquals(2, sut.find(1).next.value);
+
+        assertEquals(2, sut.find(2).value);
+        assertEquals(1, sut.find(2).prev.value);
+
+        assertEquals(3, sut.find(3).value);
+        assertEquals(4, sut.find(4).value);
+        assertEquals(5, sut.find(5).value);
+        assertEquals(6, sut.find(6).value);
+        assertEquals(7, sut.find(7).value);
+        assertEquals(8, sut.find(8).value);
+
+        assertEquals(9, sut.find(9).value);
+        assertNull(sut.find(9).next);
+
+        assertNull(sut.find(10));
+    }
+
+    @Test
+    void test_delete() {
+        OrderedList<Integer> sut = createSUTInt(true);
+        assertEquals(9, sut.count());
+        sut.delete(10);
+        sut.delete(0);
+        assertEquals(9, sut.count());
+
+        sut.delete(1);
+        assertEquals(8, sut.count());
+        assertEquals(2, sut.head.value);
+        assertNull(sut.head.prev);
+
+        sut.delete(9);
+        assertEquals(7, sut.count());
+        assertEquals(8, sut.tail.value);
+        assertNull(sut.tail.next);
+
+        sut.delete(4);
+        assertEquals(6, sut.count());
+    }
+
+    @Test
+    void test_desc_list() {
+        OrderedList<Integer> sut = createSUTInt(false);
+        assertEquals(9, sut.head.value);
+        assertEquals(0, sut.tail.value);
+    }
+
+    @Test
+    void test_desc_add() {
+        OrderedList<Integer> sut = createSUTInt(false);
+        assertEquals(9, sut.count());
+
+        sut.add(10);
+        assertEquals(10, sut.count());
+
+        assertEquals(10, sut.head.value);
+        assertEquals(9, sut.head.next.value);
+        assertEquals(10, sut.head.next.prev.value);
+
+        sut.add(0);
+        assertEquals(11, sut.count());
+
+        assertEquals(0, sut.tail.value);
+        assertEquals(1, sut.tail.prev.value);
+        assertEquals(0, sut.tail.prev.next.value);
+
+        sut.add(9);
+        assertEquals(12, sut.count());
+    }
+
+    @Test
+    void test_desc_remove() {
+        OrderedList<Integer> sut = createSUTInt(false);
+
+        sut.delete(10);
+        sut.delete(0);
+        assertEquals(9, sut.count());
+
+        sut.delete(9);
+        assertEquals(8, sut.count());
+        assertEquals(8, sut.head.value);
+        assertNull(sut.head.prev);
+
+        sut.delete(1);
+        assertEquals(7, sut.count());
+        assertEquals(2, sut.tail.value);
+        assertNull(sut.tail.next);
+
+        sut.delete(4);
+        assertEquals(6, sut.count());
+    }
+
+    @Test
+    void test_desc_find() {
+        OrderedList<Integer> sut = createSUTInt(false);
+        assertNull(sut.find(0));
+
+        assertEquals(1, sut.find(1).value);
+        assertEquals(2, sut.find(1).prev.value);
+
+        assertEquals(2, sut.find(2).value);
+        assertEquals(1, sut.find(2).next.value);
+
+        assertEquals(3, sut.find(3).value);
+        assertEquals(4, sut.find(4).value);
+        assertEquals(5, sut.find(5).value);
+        assertEquals(6, sut.find(6).value);
+        assertEquals(7, sut.find(7).value);
+        assertEquals(8, sut.find(8).value);
+
+        assertEquals(9, sut.find(9).value);
+        assertNull(sut.find(9).prev);
+
+        assertNull(sut.find(10));
+    }
+
+    @Test
+    void test_clear() {
+        OrderedList<Integer> sut = createSUTInt(false);
+        System.out.println(sut.str());
+        sut.clear(true);
+        System.out.println(sut.str());
+        sut.clear(false);
+        System.out.println(sut.str());
+    }
+
+    private OrderedList<Integer> createSUTIntEmpty(boolean asc) {
+        return new OrderedList<>(asc);
+    }
+
+    private OrderedList<Integer> createSUTInt(boolean asc) {
+        OrderedList<Integer> sut = new OrderedList<>(asc);
+
+        sut.add(1);
+        sut.add(2);
+        sut.add(3);
+        sut.add(4);
+        sut.add(5);
+        sut.add(6);
+        sut.add(7);
+        sut.add(8);
+        sut.add(9);
+
+        System.out.println("created sut: " + sut.str());
+        return sut;
+    }
+
+    private OrderedList<String> createSUTStr(boolean asc) {
+        return new OrderedList<>(asc);
+    }
+}
